c1f6fe2915ce0e4cf0e2e47ec03b807f4d57d8d0
shyamsundar06 <shyamsundarrajendiran@gmail.com>
2026-09-17 22:10:05 +0530

    Added Search In 2D Array

---
+++ Arrays/Searchin2DArray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+
+        int[][] arr = {
+            {10, 20, 30},
+            {40, 50, 60},
+            {70, 80, 90}
+        };
+
+        int target = 50;
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+
+                if (arr[i][j] == target) {
+                    System.out.println("Element found at row " + i + " and column " + j);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element not found");
+        }
+    }
+}
