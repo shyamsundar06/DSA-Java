ad5151ab719e224e0b04cdca909aec0abe45565f
shyamsundar06 <shyamsundarrajendiran@gmail.com>
2026-09-19 20:00:28 +0530

    Added Linear Search

---
+++ Arrays/Searching/LinearSearch.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+        int target = 30;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == target) {
+                System.out.println("Element found at index: " + i);
+                return;
+            }
+        }
+
+        System.out.println("Element not found");
+    }
+}
