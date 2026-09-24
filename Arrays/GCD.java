1bb1199f2d0df00f54c6e299c24bb5e2d842022a
shyamsundar06 <shyamsundarrajendiran@gmail.com>
2026-09-24 19:43:38 +0530

    Added GCD/HCF

---
+++ Arrays/GCD.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GCD {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("GCD = " + gcd(a, b));
+
+        sc.close();
+    }
+}
